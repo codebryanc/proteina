aef47a3907b36a5398e62ae45b169f2b54503c77
Bryan Cubillos <Bryant_alejandro@hotmail.com>
2020-07-24 22:18:07 -0500

    Compile!

--- src/app/modules/app/app.module.ts
+++ src/app/modules/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from '../routing/app-routing.module';
 import { AppComponent } from './components/app/app.component';
@@ -27,7 +30,10 @@ import { ProductDetailComponent } from './components/product-detail/product-deta
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [
   ],
